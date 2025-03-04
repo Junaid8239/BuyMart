c33023a03186d0f908dd5e447a3067c6d790cb46
Muhammed Junaid <muhammed.junaid@tietoevry.com>
2025-03-04 11:50:12 +0530

    Add project files.

---
+++ BuyMart/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BuyMart.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+           : base(options)
+        {
+        }
+
+
+
+
+    }
+}
